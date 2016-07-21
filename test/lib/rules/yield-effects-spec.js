ce831e2f89f006d2e94d0010a01a559217e7c3c9
Philipp Kursawe <phil.kursawe@gmail.com>
2016-07-22 00:15:25 +0200

    Add effect override check to to yield-effects

--- test/lib/rules/yield-effects-spec.js
+++ test/lib/rules/yield-effects-spec.js
@@ -10,8 +10,62 @@ function buildTest(imports, code) {
   return result
 }
 
+var ERRORS = {
+  overrides: "take overrides redux-saga effect with the same name",
+  noyield: "take effect must be yielded"
+}
+
 ruleTester.run("yield-effects", rule, {
   valid: [
+    {
+      code: "import { take } from 'redux-saga';"
+          + "const sagaTake = take;"
+          + "export default function* saga() {"
+          + "  yield sagaTake();"
+          + "}"
+    },
+    {
+      code: "import { take } from 'redux-saga';"
+          + "export default function* saga() {"
+          + "  const sagaTake = take;"
+          + "  yield sagaTake();"
+          + "}"
+    },
+    {
+      code: "import { take as sagaTake } from 'redux-saga';"
+          + "const sagaTakeCreator = sagaTake;"
+          + "export default function* saga() {"
+          + "  yield sagaTakeCreator();"
+          + "}"
+    },
+    {
+      code: "import { take as sagaTake } from 'redux-saga';"
+          + "export default function* saga() {"
+          + "  const sagaTakeCreator = sagaTake;"
+          + "  yield sagaTakeCreator();"
+          + "}"
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'", "yield notAnEffect()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'", "const take = () => {}; yield sagaTake()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'", "function take() {} yield sagaTake()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'", "var take = function() {}; yield sagaTake()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'; const take = () => {};", "yield sagaTake()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'; function take() {}", "yield sagaTake()")
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'; var take = function() {};", "yield sagaTake()")
+    },
     {
       code: buildTest("import { take } from 'redux-saga'", "yield take('ACTION')")
     },
@@ -25,9 +79,73 @@ ruleTester.run("yield-effects", rule, {
   ],
   invalid: [
     {
+      code: "import { take } from 'redux-saga';"
+          + "export default function* saga() {"
+          + "  const sagaTakeEffect = take();"
+          + "  yield sagaTakeEffect;"
+          + "}",
+      errors: [ERRORS.noyield]
+    },
+    {
+      code: "import { take as sagaTake } from 'redux-saga';"
+          + "export default function* saga() {"
+          + "  const sagaTakeEffect = sagaTake();"
+          + "  yield sagaTakeEffect;"
+          + "}",
+      errors: ["sagaTake effect must be yielded"]
+    },
+    // overrides
+    {
+      code: buildTest("import { take } from 'redux-saga'", "const take = () => {}; yield take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; const take = () => {}", "yield take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; var take = function() {}", "yield take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; const take = function() {}", "yield take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; function take() {}", "yield take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'", "const take = () => {}; take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; const take = () => {}", "take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; var take = function() {}", "take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; const take = function() {}", "take()"),
+      errors: [ERRORS.overrides]
+    },
+    {
+      code: buildTest("import { take } from 'redux-saga'; function take() {}", "take()"),
+      errors: [ERRORS.overrides]
+    },
+
+    // no yield
+    {
       code: buildTest("import { take } from 'redux-saga'", "take('ACTION')"),
       output: buildTest("import { take } from 'redux-saga'", "yield take('ACTION')"),
-      errors: [{message: "take effect must be yielded"}]
+      errors: [ERRORS.noyield]
+    },
+    {
+      code: buildTest("import { take as sagaTake } from 'redux-saga'", "sagaTake('ACTION')"),
+      output: buildTest("import { take as sagaTake } from 'redux-saga'", "yield sagaTake('ACTION')"),
+      errors: ["sagaTake effect must be yielded"]
     }
   ]
 })
