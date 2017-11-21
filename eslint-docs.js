d8234a7806007b6a244973fe64a37029f5a8c227
Philipp Kursawe <phil.kursawe@gmail.com>
2017-11-21 13:36:06 +0100

    WIP: Start rules doc

---
+++ eslint-docs.js
@@ -0,0 +1,18 @@
+/**
+ * This script searches all `lib/rules/*.js` rules.
+ * It extracts the exported `meta.docs` info.
+ * Then finds the corrosponding test in beside the rule and in test/lib/rules
+ *
+ * category: meta.docs.category
+ * ---
+ * # meta.docs.description (rule-name)
+ *
+ * ![](https://img.shields.io/badge/-recommended-lightgrey.svg "recommended") ![fixable](https://img.shields.io/badge/-fixable-green.svg "The `--fix` option on the command line automatically fixes problems reported by this rule.")
+ *
+ * @desc
+ * @since
+ * @deprecated
+  *
+ * { tests }
+ *
+ */
